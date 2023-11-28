888e8e92177fa74526da0bbc5a62c7472bcab535
Holly Hsiao <yahanhsiao@gmail.com>
2023-11-28 23:14:59 +0800

    feat: complete welcome page

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -1,13 +1,28 @@
+import { useState } from 'react';
 import { Button } from 'primereact/button';
+import { Image } from 'primereact/image';
 import { useNavigate } from 'react-router-dom';
+import angryFace from '../assets/angry.png'
+import whatFace from '../assets/what.png'
+import reactSvg from '../assets/react.svg'
 
 function Welcome() {
   const navigate = useNavigate();
+  const [currentImage, setCurrentImage] = useState(angryFace);
 
   return (
     <>
-      <p>Welcome</p>
-      <p>Sign in first</p>
+      <h1>Mood Journal</h1>
+      <div className="card flex justify-content-between">
+        <Image 
+          src={currentImage} 
+          alt="Image" 
+          height="500"
+          onMouseEnter={() => setCurrentImage(whatFace)}
+          onMouseLeave={() => setCurrentImage(angryFace)} 
+        />
+        <Image src={reactSvg} alt="Image" width="300" />
+      </div>
       <div className="card flex justify-content-center">
         <Button
           label="Sign In or Sign Up >>>"
