36eeeb4bb9d890886a2d123ca117b1733cb73964
Holly Hsiao <yahanhsiao@gmail.com>
2023-12-08 14:09:19 +0800

    test: add k6 test file

---
+++ server/test/test.js
@@ -0,0 +1,72 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+import {
+  GET_JOURNALS_BY_USER,
+  GET_JOURNAL_ID_BY_TITLE,
+  GET_BACKLINK,
+  GET_AUTOCOMPLETE,
+  SEARCH_JOURNALS,
+} from './testConf.js';
+
+const url = 'https://mood-journal.holly-hsiao.com/graphql';
+const token =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTU0YWQ1NjM1NmYzMzI1OWYxNWIxMDMiLCJuYW1lIjoidGVzdCIsImVtYWlsIjoidGVzdEB0ZXN0LmNvbSIsImlhdCI6MTcwMjAxNTE1MSwiZXhwIjoxNzAyMTAxNTUxfQ.oAhtge5OKUdcHrbUbiPIldTNVJGhrQxz2KS9gUoSEXQ';
+
+// === GET_JOURNALS_BY_USER ===
+const query = GET_JOURNALS_BY_USER;
+const payload = { query, operationName: 'GetJournalsbyUserId' };
+
+// === GET_JOURNAL_ID_BY_TITLE ===
+// const query = GET_JOURNAL_ID_BY_TITLE;
+// const variables = { title: '子華' };
+// const payload = { query, variables, operationName: 'GetJournalbyTitle' };
+
+// === GET_BACKLINK ===
+// const query = GET_BACKLINK;
+// const variables = { id: '65570b7a77238dc23a63ac77' };
+// const payload = { query, variables, operationName: 'GetBackLinkedJournals' };
+
+// === GET_AUTOCOMPLETE ===
+// const query = GET_AUTOCOMPLETE;
+// const variables = { keyword: 'Ap' };
+// const payload = { query, variables, operationName: 'AutoCompleteJournals' };
+
+// === SEARCH_JOURNALS ===
+// const query = SEARCH_JOURNALS;
+// const variables = { keyword: '奇峻' };
+// const payload = { query, variables, operationName: 'SearchJournals' };
+
+const headers = {
+  Authorization: token,
+  'Content-Type': 'application/json',
+};
+
+export const options = {
+  // == smoke ===
+  // vus: 3,
+  // duration: '3s',
+  // == load ==
+  // stages: [
+  //   { duration: '5m', target: 100 }, // traffic ramp-up from 1 to 100 users over 5 minutes.
+  //   { duration: '30m', target: 100 }, // stay at 100 users for 30 minutes
+  //   { duration: '5m', target: 0 }, // ramp-down to 0 users
+  // ],
+  // == stress ==
+  stages: [
+    { duration: '10m', target: 200 }, // traffic ramp-up from 1 to a higher 200 users over 10 minutes.
+    // { duration: '30m', target: 200 }, // stay at higher 200 users for 30 minutes
+    // { duration: '5m', target: 0 }, // ramp-down to 0 users
+  ],
+};
+
+export default () => {
+  const urlRes = http.post(url, JSON.stringify(payload), { headers });
+  let responseBody = JSON.parse(urlRes.body);
+  // console.log('Response body:', responseBody);
+  // console.log('Data:', responseBody.data);
+  // console.log('Errors:', responseBody.errors);
+  check(urlRes, {
+    'status was 200 and without errors': (r) => r.status == 200 && !JSON.parse(r.body).errors,
+  });
+  sleep(1);
+};
--- server/test/testConf.js
+++ server/test/testConf.js
@@ -1,14 +1,73 @@
 export const testUsers = [
-  // tester 1 - 5
-  '657174b1f19695d6e72c3c75',
-  '6566d280768cb89b820f2a17',
-  '6566d342768cb89b820f2a1c',
-  '6566d386768cb89b820f2a1f',
-  '6566d3ff768cb89b820f2a22',
-  // tester 6 - 10
-  '657174d8f19695d6e72c3c7b',
-  '657174eff19695d6e72c3c90',
-  '65717503f19695d6e72c3c96',
-  '65717513f19695d6e72c3c9c',
-  '65717528f19695d6e72c3ca2',
+  // == tester 1 - 5 ==
+  // '657174b1f19695d6e72c3c75',
+  // '6566d280768cb89b820f2a17',
+  // '6566d342768cb89b820f2a1c',
+  // '6566d386768cb89b820f2a1f',
+  // '6566d3ff768cb89b820f2a22',
+  // == tester 6 - 10 ==
+  // '657174d8f19695d6e72c3c7b',
+  // '657174eff19695d6e72c3c90',
+  // '65717503f19695d6e72c3c96',
+  // '65717513f19695d6e72c3c9c',
+  // '65717528f19695d6e72c3ca2',
 ];
+
+export const GET_JOURNALS_BY_USER = `
+  query GetJournalsbyUserId {
+    getJournalsbyUserId {
+      _id
+      title
+      type
+      content
+      userId
+      moodScore
+      moodFeelings
+      moodFactors
+      createdAt
+      updatedAt
+    }
+  }
+`;
+
+export const GET_JOURNAL_ID_BY_TITLE = `
+query GetJournalbyTitle($title: String!) {
+  getJournalbyTitle(title: $title) {
+    _id
+  }
+}
+`;
+
+export const GET_BACKLINK = `
+query GetBackLinkedJournals($id: ID!) {
+  getBackLinkedJournals(ID: $id) {
+    _id
+    title
+    content
+    type
+  }
+}
+`;
+
+export const GET_AUTOCOMPLETE = `
+query AutoCompleteJournals($keyword: String!) {
+  autoCompleteJournals(keyword: $keyword) {
+    title
+  }
+}
+`;
+
+export const SEARCH_JOURNALS = `
+query SearchJournals($keyword: String!) {
+  searchJournals(keyword: $keyword) {
+    _id
+    title
+    type
+    content
+    updatedAt
+    moodScore
+    moodFeelings
+    moodFactors
+  }
+}
+`;
