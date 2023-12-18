a41295fb1fb86ecb308e59259439e6dfaed1a6b0
Holly Hsiao <yahanhsiao@gmail.com>
2023-12-18 17:40:54 +0800

    feat: Beautify recording icon

--- client/src/components/AudioRecording.jsx
+++ client/src/components/AudioRecording.jsx
@@ -1,11 +1,11 @@
-import { useState, useRef } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import { Button } from 'primereact/button';
 import PropTypes from 'prop-types';
 import Cookies from 'js-cookie';
 import { toast } from 'react-toastify';
 import { MAX_AUDIO_LENGTH } from '../utils/conf';
 
-function AudioRecorder ({ setAudioNameS3 }) {
+function AudioRecorder({ setAudioNameS3 }) {
   const [stateIndex, setStateIndex] = useState(0);
   const [mediaRecorder, setMediaRecorder] = useState(null);
   const audioChunksRef = useRef([]);
@@ -19,36 +19,31 @@ function AudioRecorder ({ setAudioNameS3 }) {
         .then((stream) => {
           const recorder = new MediaRecorder(stream);
           audioChunksRef.current = [];
-
           recorder.ondataavailable = (e) => {
             audioChunksRef.current.push(e.data);
           };
-
           recorder.onstop = () => {
             const blob = new Blob(audioChunksRef.current, { type: 'audio/webm' });
             setAudioURL(window.URL.createObjectURL(blob));
             audioChunksRef.current = [];
-            stream.getTracks().forEach(track => track.stop());
+            stream.getTracks().forEach((track) => track.stop());
           };
-
           setMediaRecorder(recorder);
           setStateIndex(1);
           recorder.start();
-
           setTimeout(() => {
             if (recorder.state === 'recording') {
               recorder.stop();
               setStateIndex(2);
             }
           }, MAX_AUDIO_LENGTH);
-
         })
         .catch((error) => {
           console.error(error);
           setStateIndex('');
         });
     } else {
-      console.log('Your browser does not support mediaDevices');
+      toast.error('瀏覽器不支援錄音功能');
       setStateIndex('');
     }
   };
@@ -58,14 +53,12 @@ function AudioRecorder ({ setAudioNameS3 }) {
     mediaRecorder.stop();
   };
 
-	const uploadAudio = async () => {
+  const uploadAudio = async () => {
     try {
       const response = await fetch(audioURL);
       const blob = await response.blob();
-  
       const formData = new FormData();
       formData.append('audio', blob, 'audio.webm');
-  
       const headers = formData.getHeaders ? formData.getHeaders() : {};
       const token = Cookies.get('JWT_TOKEN');
       headers['Authorization'] = token;
@@ -73,36 +66,36 @@ function AudioRecorder ({ setAudioNameS3 }) {
       const port = hostname === 'localhost' ? ':3000' : '';
       const apiUri = `${protocol}//${hostname}${port}/api/audio`;
       const res = await fetch(apiUri, {
-          method: 'POST',
-          body: formData,
-          headers,
-      })
+        method: 'POST',
+        body: formData,
+        headers,
+      });
       const data = await res.json();
       if (data.fileName) {
         setStateIndex(0);
+        toast.success('音檔上傳成功');
         return setAudioNameS3(data.fileName);
       }
       throw new Error(data.error);
     } catch (error) {
       toast.error('無法上傳音檔');
-      console.error(error)
+      console.error(error);
     }
-
-};
+  };
 
   return (
     <div>
-      <h3>錄音</h3>
+      <h3 className='mb-0'>錄音</h3>
       <div className="display">
         {stateIndex === 1 && <p>錄音中...</p>}
         {stateIndex === 2 && <audio controls src={audioURL} preload="auto"></audio>}
       </div>
       <br/>
       <div className="controllers">
-        {stateIndex === 0 && <Button onClick={record} severity="secondary" label="開始錄音" />}
-        {stateIndex === 1 && <Button onClick={stopRecording} severity="secondary" label="停止錄音" />}
-        {stateIndex === 2 && <Button onClick={record} severity="secondary" label="重新錄音" />}
-        {stateIndex === 2 && <Button onClick={uploadAudio} severity="secondary" label="上傳" />}
+        {stateIndex === 0 && <Button onClick={record} className='mr-1' severity="secondary" icon="pi pi-circle-fill" aria-label="開始錄音" />}
+        {stateIndex === 1 && <Button onClick={stopRecording} className='mr-1' severity="secondary" icon="pi pi-stop" aria-label="停止錄音" />}
+        {stateIndex === 2 && <Button onClick={record} className='mr-1' severity="secondary" icon="pi pi-refresh" aria-label="重新錄音" />}
+        {stateIndex === 2 && <Button onClick={uploadAudio} className='mr-1' severity="secondary" icon="pi pi-upload" aria-label="上傳" />}
       </div>
     </div>
   );
