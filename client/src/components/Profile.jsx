57f788fb6fc3b95be8b7918e56493c53997ce253
Holly Hsiao <yahanhsiao@gmail.com>
2023-12-19 10:40:17 +0800

    feat: Add avatar for profile page

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -3,6 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import Cookies from 'js-cookie';
 import { useUserState } from '../state/state.js';
 import { toast } from 'react-toastify';
+import { Image } from 'primereact/image';
 
 function Profile() {
   const navigate = useNavigate();
@@ -17,15 +18,28 @@ function Profile() {
   };
 
   return (
-    <>
-      <h1 className="text-left ml-8">基本資料</h1>
-      <div className="flex flex-wrap flex-column text-left ml-8">
-        <p>{`Id: ${userInfoState.id}`}</p>
-        <p>{`帳號: ${userInfoState.name}`}</p>
-        <p>{`電子信箱: ${userInfoState.email}`}</p>
+    <div style={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+    }}>
+      <div>
+        <h1 className="text-left">基本資料</h1>
+        <div className="flex">
+          <Image
+            src={`https://api.dicebear.com/7.x/thumbs/svg?seed=${userInfoState.name}`}
+            alt="avatar"
+            height="150px"
+          />
+          <div className="flex flex-wrap flex-column text-left ml-5">
+            <p>{`Id: ${userInfoState.id}`}</p>
+            <p>{`帳號: ${userInfoState.name}`}</p>
+            <p>{`電子信箱: ${userInfoState.email}`}</p>
+          </div>
+        </div>
         <Button label="登出" onClick={signOut} />
       </div>
-    </>
+    </div>
   );
 }
 
